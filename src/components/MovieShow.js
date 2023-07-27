97c81202cbf865c6b3d2345238840adf1827aaa0
iam <brian.kibet2@student.moringaschool.com>
2023-07-27 08:53:29 +0000

    task completed

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+// ./src/components/MovieShow.js
+import React from "react";
+
+function MovieShow() {
+  return (
+    <div>
+      <h3>Movies Show Component!</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
